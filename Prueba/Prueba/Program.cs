1d8684b875456a65fb02fa32ccd5b59c42e271d3
yilber.calderon <yilberandrey@gmail.com>
2017-03-24 13:53:23 -0500

    hola mundo

--- Prueba/Prueba/Program.cs
+++ Prueba/Prueba/Program.cs
@@ -10,6 +10,8 @@ namespace Prueba
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola mundo");
+            //Aqui vamamos a tener nuestro main
         }
     }
 }
